31367a9c2c27cb43eb081f6703b6518be446c73b
Anas Khafegui <anas.mdhat@outlook.com>
2022-05-09 18:18:46 +0200

    implement category endpoint functionality

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\CategoryResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'children' => CategoryResource::collection($this->whenLoaded('children'))
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/category', [CategoryController::class, 'index']);
+
+
+Route::get('/category/{id}', [CategoryController::class, 'show']);
